bb79caf52a9cf575680aafa217f9957015f19ca6
theDevIsALie <50375282+theDevIsALie@users.noreply.github.com>
2019-10-05 21:23:04 +0200

    Add files via upload

--- utilities.py
+++ utilities.py
@@ -1,6 +1,17 @@
 import bpy
 import sys
 
+# Default RGBA (when RGBA is missing)
+default_r = 255
+default_g = 255
+default_b = 255
+default_a = 255
+
+# Default X Y Z position (when X Y Z are missing)
+default_x = 0.0
+default_y = 0.0
+default_z = 0.0
+
 
 def get_rgba(crt_line, crt_line_num, index, op):
     # Red value (r)
@@ -10,16 +21,16 @@ def get_rgba(crt_line, crt_line_num, index, op):
             int(crt_line[index])
             if int(crt_line[index]) < 0 or int(crt_line[index]) > 255:
                 op.report({'ERROR'}, f"Red value should be between 0-255 on line {crt_line_num}")
-                r = 255
+                r = default_r
             else:
                 # r value defined correctly
                 r = int(crt_line[index])
         except:
             op.report({'ERROR'}, f"Red value should be an int on line {crt_line_num}")
-            r = 255
+            r = default_r
     except:
         op.report({'ERROR'}, f"Missing red value on line {crt_line_num}")
-        r = 255
+        r = default_r
 
     # Green value (g)
     try:
@@ -28,16 +39,16 @@ def get_rgba(crt_line, crt_line_num, index, op):
             int(crt_line[index + 1])
             if int(crt_line[index + 1]) < 0 or int(crt_line[index + 1]) > 255:
                 op.report({'ERROR'}, f"Green value should be between 0-255 on line {crt_line_num}")
-                g = 255
+                g = default_g
             else:
                 # g value defined correctly
                 g = int(crt_line[index + 1])
         except:
             op.report({'ERROR'}, f"Green value should be an int on line {crt_line_num}")
-            g = 255
+            g = default_g
     except:
         op.report({'ERROR'}, f"Missing green value on line {crt_line_num}")
-        g = 255
+        g = default_g
 
     # Blue value (b)
     try:
@@ -46,16 +57,16 @@ def get_rgba(crt_line, crt_line_num, index, op):
             int(crt_line[index + 2])
             if int(crt_line[index + 2]) < 0 or int(crt_line[index + 2]) > 255:
                 op.report({'ERROR'}, f"Blue value should be between 0-255 on line {crt_line_num}")
-                b = 255
+                b = default_b
             else:
                 # b value defined correctly
                 b = int(crt_line[index + 2])
         except:
             op.report({'ERROR'}, f"Blue value should be an int on line {crt_line_num}")
-            b = 255
+            b = default_b
     except:
         op.report({'ERROR'}, f"Missing blue value on line {crt_line_num}")
-        b = 255
+        b = default_b
 
     # Alpha value (a)
     try:
@@ -64,16 +75,16 @@ def get_rgba(crt_line, crt_line_num, index, op):
             int(crt_line[index + 3])
             if int(crt_line[index + 3]) < 0 or int(crt_line[index + 3]) > 255:
                 op.report({'ERROR'}, f"Alpha value should be between 0-255 on line {crt_line_num}")
-                a = 255
+                a = default_a
             else:
                 # a value defined correctly
                 a = int(crt_line[index + 3])
         except:
             op.report({'ERROR'}, f"Alpha value should be an int on line {crt_line_num}")
-            a = 255
+            a = default_a
     except:
         op.report({'ERROR'}, f"Missing alpha value on line {crt_line_num}")
-        a = 255
+        a = default_a
 
     # Convert all the values into a blender color value (0 -> 1)
     red = r / 255
@@ -86,6 +97,7 @@ def get_rgba(crt_line, crt_line_num, index, op):
 
 def get_xyz(crt_line, crt_line_num, index, op):
     # Positions of the voxel
+
     # X position (x)
     try:
         _x = crt_line[index]
@@ -93,10 +105,10 @@ def get_xyz(crt_line, crt_line_num, index, op):
             x = float(crt_line[index])
         except:
             op.report({'ERROR'}, f"x should be a float on line {crt_line_num}")
-            x = 0
+            x = default_x
     except:
         op.report({'ERROR'}, f"x value is missing on line {crt_line_num}")
-        x = 0
+        x = default_x
 
     # Y position (y)
     try:
@@ -105,10 +117,10 @@ def get_xyz(crt_line, crt_line_num, index, op):
             y = float(crt_line[index + 1])
         except:
             op.report({'ERROR'}, f"y value should be a float on line {crt_line_num}")
-            y = 0
+            y = default_y
     except:
         op.report({'ERROR'}, f"y value is missing on line {crt_line_num}")
-        y = 0
+        y = default_y
 
     # Z position (z)
     try:
@@ -117,9 +129,9 @@ def get_xyz(crt_line, crt_line_num, index, op):
             z = float(crt_line[index + 2])
         except:
             op.report({'ERROR'}, f"z value should be a float on line {crt_line_num}")
-            z = 0
+            z = default_z
     except:
         op.report({'ERROR'}, f"z value is missing on line {crt_line_num}")
-        z = 0
+        z = default_z
 
     return x, y, z
